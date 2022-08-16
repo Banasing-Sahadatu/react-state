3fe938c75232d3dc58e8b765dd61a507b5f68a77
Banasing-Sahadatu <banasingsahadatu@gmail.com>
2022-08-16 17:49:42 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { render } from '@testing-library/react';
+import React, {Component} from 'react'
+import ProfileCard from './component/ProfileCard'
 
-function App() {
+class App extends Component {
+  constructor() {
+  supper();
+
+this.handleClick=this.handleClick.bind(this)
+
+  this.state = {
+    writer: {
+      loading: false,
+      list:[]
+    }
+  }
+
+  this.handleClick(){
+    this.setState({
+      writers: {
+        loading: true,
+      }
+    });
+    setTimeout(async () => {
+      let resp = await ("/writers.json");
+      let result = await resp.json()
+    })
+
+    this.setState({
+      writers: {
+        loading: false,
+        list:result
+      }
+    })
+
+  
+    }
+
+  }
+
+
+
+render() {
+  const {
+    writers: { loading, list }
+  } = this.state;
+}
+if(loading) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <h1>writers profile</h1>
+      <div className='container'>
+        <div className='card action'>
+          <p className='infotext'> loading...</p>
+        </div>
+      </div>
     </div>
-  );
+  )
+}
 }
 
-export default App;
+export default App
